36726e67ee3e51c1868000e8e075bb79639d7b4b
Melissa Choy <mel@otilas.com.au>
2013-03-23 00:12:53 +1100

    Update url

--- lib/radiant-slideshow-extension.rb
+++ lib/radiant-slideshow-extension.rb
@@ -2,7 +2,7 @@ module RadiantSlideshowExtension
   VERSION     = "1.0.0"
   SUMMARY     = "Slideshow for Radiant CMS"
   DESCRIPTION = "Makes Radiant better by adding a slideshow!" 
-  URL         = "http://www.otilas.com.au"
+  URL         = "https://github.com/melchoy/radiant-slideshow-extension"
   AUTHORS     = ["Melissa Choy"]
   EMAIL       = ["mel@otilas.com.au"]
 end
